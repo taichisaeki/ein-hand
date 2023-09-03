754a3d9f183d5a50417d903b5e610436c4e2b502
taitai0831 <taichi.saeki831@gmail.com>
2023-09-03 16:05:55 +0900

    change maxboxes

--- hands.js
+++ hands.js
@@ -11,7 +11,7 @@ let model = null;
 
 const modelParams = {
   flipHorizontal: true, // flip e.g for video
-  maxNumBoxes: 20, // maximum number of boxes to detect
+  maxNumBoxes: 1, // maximum number of boxes to detect
   iouThreshold: 0.5, // ioU threshold for non-max suppression
   scoreThreshold: 0.6, // confidence threshold for predictions.
 };
